5a346d891fb0815052803ed565d5c9897cc7652d
samTorres <samueltorres@Outlook.com>
2022-06-04 10:40:04 -0500

    changed configuration of desktop and mobile images

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,21 +1,23 @@
 import * as React from "react"
 import { graphql } from 'gatsby'
-import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+
 import Gallery from '@browniebroke/gatsby-image-gallery'
-import Header from "../components/header"
+
 
 
 const SecondPage = ({ data }) => {
   const images = data.allFile.edges.map(({ node }) => node.childImageSharp)
   // `images` is an array of objects with `thumb` and `full`
   return <Layout>
+
+    
       <Gallery
-        images={images}
-        mdColWidth={33}
+        imag={images}
+        mdColWidth={20}
         gutter={".25rem"}
         colWidth={100}
+        
       />
     </Layout>  
 }
